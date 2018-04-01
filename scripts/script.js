47ac8fcd198c6ca8b7ffa5227ca3b43d5877d83f
vitalities <vitalik.mokiychuk@gmail.com>
2018-04-01 16:53:22 +0300

    updated script.js,  html, css

--- scripts/script.js
+++ scripts/script.js
@@ -5,32 +5,61 @@ $('.year_link').click(function() {
   $( this ).addClass('year_link__this-page');
 })
 
-var carrerChangeContent = function (carrerPage_num) {
-  var carrerText_2013 = ' Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. ';
-  var carrerText_2014 = 'Labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.';
-  var carrerText_2014 = 'Qiuis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.Velit esse cillum dolore eu fugiat nulla pariatur.Velit esse cillum dolore eu fugiat nulla pariatur.Velit esse cillum dolore eu fugiat nulla pariatur.';
-  var carrerText_2015 = 'Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. ';
-  var carrerText_2016 = 'In voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. ';
-  var carrerText_2017 = 'In voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. ';
-  var carrerText_2018 = 'In voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. ';
+$('.year_2013').click(function() {
+  careerChangeContent('careerPage_2013');
+})
+$('.year_2014').click(function() {
+  careerChangeContent('careerPage_2014');
+})
+$('.year_2015').click(function() {
+  careerChangeContent('careerPage_2015');
+})
+$('.year_2016').click(function() {
+  careerChangeContent('careerPage_2016');
+})
+$('.year_2017').click(function() {
+  careerChangeContent('careerPage_2017');
+})
+$('.year_2018').click(function() {
+  careerChangeContent('careerPage_2018');
+})
 
-  var carrerImage = $('.info__carrer__photo__picture__img');
-  if (carrerPage_num === carrerPage_2013) {
-    carrerImage.attr('src','images/pictures/carrer_2013.jpg')
+var careerChangeContent = function(careerPage_num) {
+  var careerText_2013 = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. ';
+  var careerText_2014 = 'Labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.';
+  var careerText_2014 = 'Qiuis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.Velit esse cillum dolore eu fugiat nulla pariatur.Velit esse cillum dolore eu fugiat nulla pariatur.Velit esse cillum dolore eu fugiat nulla pariatur.';
+  var careerText_2015 = 'Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. ';
+  var careerText_2016 = 'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. ';
+  var careerText_2017 = 'In voluptate velit esse  sunt in culpa qui officia deserunt mollit anim id est laborum. ';
+  var careerText_2018 = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos facere iusto molestiae ab dolorum delectus exercitationem dicta quaerat. Accusantium sit nostrum veniam eligendi qui ipsa, cupiditate. Quae itaque, voluptatibus officia.';
+  var careerImage = $('.info__career__photo__picture__img');
+  var careerText = $('.info__career__about-text');
+  if (careerPage_num === 'careerPage_2013') {
+    careerImage.attr('src','images/pictures/career_2013.jpg');
+    careerText.text(careerText_2013);
   }
-  if (carrerPage_num === carrerPage_2014) {
-    carrerImage.attr('src','images/pictures/carrer_2014.jpg')
+  if (careerPage_num === 'careerPage_2014') {
+    careerImage.attr('src','images/pictures/career_2014.jpg');
+    careerText.text(careerText_2014);
   }
-  if (carrerPage_num === carrerPage_2015) {
-    carrerImage.attr('src','images/pictures/carrer_2015.jpg')
+  if (careerPage_num === 'careerPage_2015') {
+    careerImage.attr('src','images/pictures/career_2015.jpg');
+    careerText.text(careerText_2015);
   }
-  if (carrerPage_num === carrerPage_2016) {
-    carrerImage.attr('src','images/pictures/carrer_2016.jpg')
+  if (careerPage_num === 'careerPage_2016') {
+    careerImage.attr('src','images/pictures/career_2016.jpg');
+    careerText.text(careerText_2016);
   }
-  if (carrerPage_num === carrerPage_2017) {
-    carrerImage.attr('src','images/pictures/carrer_2017.jpg')
+  if (careerPage_num === 'careerPage_2017') {
+    careerImage.attr('src','images/pictures/career_2017.jpg');
+    careerText.text(careerText_2017);
   }
-  if (carrerPage_num === carrerPage_2018) {
-    carrerImage.attr('src','images/pictures/carrer_2018.jpg')
+  if (careerPage_num === 'careerPage_2018') {
+    careerImage.attr('src','images/pictures/career_2018.jpg');
+    careerText.text(careerText_2018);
   }
 }
+
+$('.contacts__list').click(function() {
+  $('.contacts__dropdown').slideToggle();
+})
