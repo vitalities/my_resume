cf97387fa8df0b088af73b876aadf4ecb1c2dbb9
vitalities <vitalik.mokiychuk@gmail.com>
2018-04-14 22:04:58 +0300

    added page "skills"

---
+++ scripts/skills_script.js
@@ -0,0 +1,51 @@
+var i = true;
+
+while (i == true) {
+  var
+  p1 = $('.skill-1');
+
+  height1 = ( 100 * parseFloat($('.filling-1').css('height')) / parseFloat($('.filling-1').parent().css('height')) ) + '%';
+
+  p1.text(height1);
+  console.log(height1);
+  i = false;
+}
+
+var i = true;
+
+while (i == true) {
+  var
+  p2 = $('.skill-2');
+
+  height2 = ( 100 * parseFloat($('.filling-2').css('height')) / parseFloat($('.filling-2').parent().css('height')) ) + '%';
+
+  p2.text(height2);
+  console.log(height2);
+  i = false;
+}
+
+var i = true;
+
+while (i == true) {
+  var
+  p3 = $('.skill-3');
+
+  height3 = ( 100 * parseFloat($('.filling-3').css('height')) / parseFloat($('.filling-3').parent().css('height')) ) + '%';
+
+  p3.text(height3);
+  console.log(height3);
+  i = false;
+}
+
+var i = true;
+
+while (i == true) {
+  var
+  p4 = $('.skill-4');
+
+  height4 = ( 100 * parseFloat($('.filling-4').css('height')) / parseFloat($('.filling-4').parent().css('height')) ) + '%';
+
+  p4.text(height4);
+  console.log(height4);
+  i = false;
+}
